ad54efafc98980fbb53498f093fef23f1492366b
desiderium-wav <g09784530@gmail.com>
2026-01-26 15:23:48 -0500

    Add Giphy and Tenor configuration options

--- gif_engine/config.py
+++ gif_engine/config.py
@@ -17,5 +17,20 @@ E621_API_KEY = os.getenv("E621_API_KEY", None)
 E621_USER_AGENT = os.getenv("E621_USER_AGENT", "word-coutner-bot/1.0 (by username)")
 
 ENABLE_NEKOS = os.getenv("ENABLE_NEKOS", "true").lower() in ("1", "true", "yes")
-# Candidate neko endpoints; some are SFW, some NSFW. We'll filter NSFW when allow_nsfw=False.
 NEKOS_ENDPOINTS = os.getenv("NEKOS_ENDPOINTS", "ngif,neko,smug,pat,kiss,hug").split(",")
+
+ENABLE_GIPHY = os.getenv("ENABLE_GIPHY", "false").lower() in ("1", "true", "yes")
+GIPHY_API_KEY = os.getenv("GIPHY_API_KEY", None)
+GIPHY_SEARCH_LIMIT = int(os.getenv("GIPHY_SEARCH_LIMIT", "50"))
+
+ENABLE_TENOR = os.getenv("ENABLE_TENOR", "false").lower() in ("1", "true", "yes")
+TENOR_API_KEY = os.getenv("TENOR_API_KEY", None)
+TENOR_SEARCH_LIMIT = int(os.getenv("TENOR_SEARCH_LIMIT", "50"))
+
+# If True, external APIs/cache are consulted before local DB
+PREFER_EXTERNAL = os.getenv("PREFER_EXTERNAL", "false").lower() in ("1", "true", "yes")
+# Order to try external APIs when falling back
+EXTERNAL_API_ORDER = os.getenv("EXTERNAL_API_ORDER", "e621,giphy,tenor,nekos").split(",")
+
+# Cache TTL for external API results (days)
+CACHE_TTL_DAYS = int(os.getenv("CACHE_TTL_DAYS", "7"))
